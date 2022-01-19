4777831fadfe5da311bf9eebc5793520aa627c7e
SergeyZinkovich <my_agent-1@mail.ru>
2022-01-19 23:38:14 +1000

    Add EGE B10

--- gen.py
+++ gen.py
@@ -3,7 +3,7 @@ import sys
 import EGE.GenBase
 import EGE.Html
 import EGE.Random
-from EGE.Gen.EGE import A01, A02, A03, A04, A05, A07, A08, A09, A10, A11, A12, A13, A15, A16, A17, A18, B01, B02, B03, B04, B05, B06
+from EGE.Gen.EGE import A01, A02, A03, A04, A05, A07, A08, A09, A10, A11, A12, A13, A15, A16, A17, A18, B01, B02, B03, B04, B05, B06, B10
 
 rnd = EGE.Random.Random(2342134)
 
@@ -83,6 +83,7 @@ questions = [q.generate() for q in [
     B06.Solve(rnd),
     B06.RecursiveFunction(rnd),
     B06.PasswordMeta(rnd),
+    B10.DirectInput(rnd),
 ]]
 
 if not sys.stdout.isatty():
