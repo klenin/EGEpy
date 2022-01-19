9b9d9819a101e0ca6330463b41a56e60b74ce2c5
Anton Podchuvalov <podchuvalovvl@gmail.com>
2022-01-20 00:48:18 +1000

    Improve code

--- EGE/Gen/EGE/A06.py
+++ EGE/Gen/EGE/A06.py
@@ -8,10 +8,10 @@ class CountBySign(SingleChoice):
     def generate(self):
         array_length = self.rnd.in_range(50, 100)
         iteration_variable = self.rnd.index_var()[0]
-        c_language_comment = unpre(
-            f'''/* В программе на языке Си следует считать, что массивы A и B
-            индексируются начиная с 1 и состоят из элементов
-            A[1], … A[{array_length}], B[1], … B[{array_length}] */'''
+        c_language_comment = unpre(f'''
+/* В программе на языке Си следует считать, что массивы A и B
+индексируются начиная с 1 и состоят из элементов
+A[1], … A[{array_length}], B[1], … B[{array_length}] */'''
         )
         first_loop_body = [
             '=', ['[]', 'A', iteration_variable], [ '*', iteration_variable, iteration_variable ],
@@ -27,21 +27,22 @@ class CountBySign(SingleChoice):
         code_block = make_block(code_block_parts)
         lang_table = table(code_block, [ [ 'Basic', 'Alg' ], [ 'Pascal', 'C' ] ])
         case = self.rnd.pick([
-            { 'name': 'положительные', 'test': lambda x: x > 0 },
-            { 'name': 'отрицательные', 'test': lambda x: x < 0 },
+            { 'name': 'положительные',   'test': lambda x: x > 0  },
+            { 'name': 'отрицательные',   'test': lambda x: x < 0  },
             { 'name': 'неотрицательные', 'test': lambda x: x >= 0 },            
         ])
-        self.text = f'''Значения двух массивов A и B с индексами от 1 до {array_length}
-            задаются при помощи следующего фрагмента программы: {lang_table}
-            Какое количество элементов массива B[1..{array_length}] будет принимать
-            {case['name']} значения после выполнения данной программы?'''
+        self.text = f'''
+Значения двух массивов A и B с индексами от 1 до {array_length}
+задаются при помощи следующего фрагмента программы: {lang_table}
+Какое количество элементов массива B[1..{array_length}] будет принимать
+{case['name']} значения после выполнения данной программы?'''
 
         array_B = code_block.run_val('B')
         correct = len(list(filter(lambda x: case['test'](x), array_B.values())))
         errors = [
             correct + 1, correct - 1, array_length - correct, array_length - correct + 1, array_length - correct - 1
         ]
-        self.set_variants([correct] + self.rnd.pick_n(3, errors))
+        self.set_variants([ correct ] + self.rnd.pick_n(3, errors))
         return self
 
 class FindMinMax(SingleChoice):
@@ -50,15 +51,15 @@ class FindMinMax(SingleChoice):
         # Нужно гарантировать единственные максимум и минимум.
         value = self.rnd.in_range(array_length // 2 + 1, array_length - 1)
         iteration_variable = self.rnd.index_var()[0]
-        d1 = [ '-' ] + self.rnd.shuffle([ iteration_variable, value ])
-        d2, d3 = self.rnd.shuffle([
-            iteration_variable, [ '-', array_length + 1, iteration_variable]
+        factor = [ '-' ] + self.rnd.shuffle([ iteration_variable, value ])
+        indexB, indexA = self.rnd.shuffle([
+            iteration_variable, [ '-', array_length + 1, iteration_variable ]
         ])
         first_loop_body = [
-            '=', [ '[]', 'A', iteration_variable ], [ '*', d1, d1 ],
+            '=', [ '[]', 'A', iteration_variable ], [ '*', factor, factor ],
         ]
         second_loop_body = [
-            '=', [ '[]', 'B', d2 ], [ '[]', 'A', d3 ],
+            '=', [ '[]', 'B', indexB ], [ '[]', 'A', indexA ],
         ]
         code_block_parts = [
             'for', iteration_variable, 1, array_length, first_loop_body,
@@ -70,12 +71,14 @@ class FindMinMax(SingleChoice):
             { 'name': 'наибольшим', 'test': 1 },
             { 'name': 'наименьшим', 'test': -1 },
         ])
-        self.text = f'''Значения двух массивов A[1..{array_length}] и B[1..{array_length}]
-            задаются с помощью следующего фрагмента программы: {lang_table}
-            Какой элемент массива B будет {case['name']}?'''
+        self.text = f'''
+Значения двух массивов A[1..{array_length}] и B[1..{array_length}]
+задаются с помощью следующего фрагмента программы: {lang_table}
+Какой элемент массива B будет {case['name']}?'''
 
         # Analoge of '<=>' operator in Perl.
-        def sign(x, y):
+        # ToDo: write better implementation.
+        def sign(x: int, y: int) -> int:
             if x < y:
                 return -1
             elif x == y:
@@ -95,7 +98,7 @@ class FindMinMax(SingleChoice):
         
         seen = { correct: True }
 
-        def filter_function(value):
+        def filter_function(value: int) -> bool:
             if not (1 <= value <= array_length) or (value in seen and seen[value]):
                 return False
             seen[value] = True
@@ -106,7 +109,6 @@ class FindMinMax(SingleChoice):
         ]))
         self.set_variants([f"B[{variant}]" for variant in [ correct ] + self.rnd.pick_n(3, errors)])
         return self
-
 
 class CountOddEven(SingleChoice):
     def generate(self):
@@ -134,10 +136,11 @@ class CountOddEven(SingleChoice):
             { 'name': 'чётное', 'test': 0 },
             { 'name': 'нечётное', 'test': 1 },
         ])
-        self.text = f'''Значения двумерного массива A размера {array_length} × {array_length}
-            задаются с помощью вложенного оператора цикла
-            в представленном фрагменте программы: {lang_table}
-            Сколько элементов массива A будут принимать {case['name']} значение?'''
+        self.text = f'''
+Значения двумерного массива A размера {array_length} × {array_length}
+задаются с помощью вложенного оператора цикла
+в представленном фрагменте программы: {lang_table}
+Сколько элементов массива A будут принимать {case['name']} значение?'''
         
         array_A = code_block.run_val('A')
         correct = 0
@@ -150,7 +153,7 @@ class CountOddEven(SingleChoice):
         errors = [ correct + error for error in errors ]
         seen = { correct: True }
 
-        def filter_function(value):
+        def filter_function(value: int) -> bool:
             if (value in seen and seen[value]):
                 return False
             seen[value] = True
@@ -186,9 +189,10 @@ class AlgMinMax(SingleChoice):
             [ '=', j, 1, ] + loop_block + [ '=', 's', idx['res'] ]
         )
         lang_table = table(code_block, [ [ 'Basic', 'Pascal', 'Alg' ] ])
-        self.text = f'''Дан фрагмент программы, обрабатывающей массив A из N элементов: {lang_table}
-            Чему будет равно значение переменной s после выполнения
-            данного алгоритма, при любых значениях элементов массива A?'''
+        self.text = f'''
+Дан фрагмент программы, обрабатывающей массив A из N элементов: {lang_table}
+Чему будет равно значение переменной s после выполнения
+данного алгоритма, при любых значениях элементов массива A?'''
         if_many = f" из них, если {minmax['text']}ых элементов несколько)"
         variants = [
             f"{minmax['text'].capitalize()}ому эдементу в массиве A",
@@ -210,7 +214,7 @@ class AlgAvg(SingleChoice):
         correct = self.correct = self.rnd.in_range(1, 3)
         index_statement = [ '[]', 'A', i, ]
         condition_body = [
-            '=', 's', index_statement if correct == 3 else [ '+', 's', index_statement],
+            '=', 's', index_statement if correct == 3 else [ '+', 's', index_statement ],
             '=', j, [ '+', j, 1 ],
         ]
         condition_block = [
@@ -223,10 +227,12 @@ class AlgAvg(SingleChoice):
         code_block_parts = [ '=', 's', 0, '=', j, 0, ] + loop_block + after_loop_block
         code_block = make_block(code_block_parts)
         lang_table = table(code_block, [ [ 'Basic', 'Pascal' ], [ 'C', 'Alg' ], ])
-        self.text = f'''Дан фрагмент программы, обрабатывающей массив A из N элементов
-            (известно, что в массиве имеются {case['text']}ые элементы): {lang_table}
-            Чему будет равно значение переменной s после выполнения
-            данного алгоритма, при любых значениях элементов массива A?'''
+        self.text = f'''
+Дан фрагмент программы, обрабатывающей массив A из N элементов
+(известно, что в массиве имеются {case['text']}ые элементы): {lang_table}
+Чему будет равно значение переменной s после выполнения
+данного алгоритма, при любых значениях элементов массива A?'''
+
         variants = [
             "Среднему арифметическому всех элементов массива A",
             f"Среднему арифметическому всех {case['text']}ых элементов массива A",
@@ -285,20 +291,21 @@ class BusStation(SingleChoice):
         self.set_variants([ self._stime(v) for v in answer ])
         return self
 
-    def _gen_schedule_text(self, way, towns, init_vertices):
+    def _gen_schedule_text(self, way: dict, towns: dict, init_vertices: list) -> str:
         start_time = self._stime(5 * self.rnd.in_range(0, int(way['start'] / 5)))
-        text = f'''<p>
-            Путешественник пришел в {start_time} на автостанцию населенного пункта
-            <strong>{towns[way['from']]}</strong> и обнаружил следующее расписание автобусов для всей районной
-            сети маршрутов:
-            </p>
-            <table border="1">
-            <tr>
-            <th>Пункт отправления</th>
-            <th>Пункт прибытия</th>
-            <th>Время отправления</th>
-            <th>Время прибытия</th>
-            </tr>'''
+        text = f'''
+<p>
+    Путешественник пришел в {start_time} на автостанцию населенного пункта
+    <strong>{towns[way['from']]}</strong> и обнаружил следующее расписание автобусов для всей районной
+    сети маршрутов:
+</p>
+<table border="1">
+    <tr>
+        <th>Пункт отправления</th>
+        <th>Пункт прибытия</th>
+        <th>Время отправления</th>
+        <th>Время прибытия</th>
+    </tr>'''
         for vertex in init_vertices:
             text += "<tr>"
             text += f"<td>{towns[vertex['from']]}</td>"
@@ -307,11 +314,12 @@ class BusStation(SingleChoice):
             text += f"<td>{self._stime(vertex['fin'])}</td>"
             text += "</tr>\n"
         text += "</table>\n"
-        text += f'''<p>Определите самое раннее время, когда путешественник сможет оказаться в
-            пункте <strong>{towns[way['to']]}</strong> согласно этому расписанию.</p>'''
+        text += f'''
+<p>Определите самое раннее время, когда путешественник сможет оказаться в
+пункте <strong>{towns[way['to']]}</strong> согласно этому расписанию.</p>'''
         return text
 
-    def _random_routes(self, path_count, n):
+    def _random_routes(self, path_count: int, n: int) -> list:
         # Генерация случайных маршрутов без петель
         # |~|1|2|3| каждому целому числу от 0 до $n * ($n - 1) можно
         # |4|~|5|6| однозначно сопоставить позицию в матрице смежности
@@ -334,7 +342,7 @@ class BusStation(SingleChoice):
 
         return vertices
 
-    def _find_all_routes(self, graph, towns_count):
+    def _find_all_routes(self, graph: dict, towns_count: int) -> dict:
         # Для нахождения кратчайших расстояний используется Алгоритм Флойда — Уоршелла
         for k in range(towns_count + 1):
             for i in range(towns_count + 1):
@@ -353,7 +361,7 @@ class BusStation(SingleChoice):
                         v['to'] = w['to']
         return graph
 
-    def _stime(self, value):
+    def _stime(self, value: int) -> str:
         return str(int(value / 60 + 7)) + ":" + "{:02d}".format(value % 60)
 
 class CrcMessage(SingleChoice):
@@ -362,7 +370,7 @@ class CrcMessage(SingleChoice):
         words = 3
         
         def create_bits():
-            new_bin = [ self.rnd.coin() for x in range(1, lenght + 1) ]
+            new_bin = [ self.rnd.coin() for _ in range(1, lenght + 1) ]
             new_bin.append(0)
             bits = Bits().set_bin(new_bin, True)
             bits.set_bit(0, bits.xor_bits())
@@ -370,7 +378,7 @@ class CrcMessage(SingleChoice):
 
         original_msg = [ create_bits() for _ in range(1, words + 1) ]
         
-        def modificate_bit(bit):
+        def modificate_bit(bit: Bits) -> Bits:
             new_bit = bit.dup()
             flipped_bits = new_bit.flip(self.rnd.pick_n(self.rnd.in_range(1, 4), [ _ for _ in range(0, lenght + 1) ]))
             return flipped_bits.set_bit(self.rnd.in_range(0, lenght - 1), 1)
@@ -384,20 +392,21 @@ class CrcMessage(SingleChoice):
 
         zeroes = '0' * (lenght + 1)
 
-        def idx_to_msg(arg):
+        def idx_to_msg(arg: int) -> str:
             bits = Bits().set_size(words).set_dec(arg).get_bits()
             return ' '.join([ zeroes if bits[x] else received_msg[x].get_bin() for x in range(words) ])
 
-        self.text = f'''В некоторой информационной системе информация кодируется двоичными шестиразрядными словами.
-            При передаче данных возможны их искажения, поэтому в конец каждого слова добавляется седьмой
-            (контрольный) разряд таким образом, чтобы сумма разрядов нового слова, считая контрольный,
-            была чётной. Например, к слову 110011 справа будет добавлен 0, а к слову 101100 – 1.
-            После приёма слова производится его обработка. При этом проверяется сумма его разрядов,
-            включая контрольный. Если она нечётна, это означает, что при передаче этого слова произошёл сбой,
-            и оно автоматически заменяется на зарезервированное слово {zeroes}. Если она чётна, это означает,
-            что сбоя не было или сбоев было больше одного. В этом случае принятое слово не изменяется.
-            Исходное сообщение: <b>{original_text}</b> было принято в виде: <b>{received_text}</b>.
-            Как будет выглядеть принятое сообщение после обработки?'''
+        self.text = f'''
+В некоторой информационной системе информация кодируется двоичными шестиразрядными словами.
+При передаче данных возможны их искажения, поэтому в конец каждого слова добавляется седьмой
+(контрольный) разряд таким образом, чтобы сумма разрядов нового слова, считая контрольный,
+была чётной. Например, к слову 110011 справа будет добавлен 0, а к слову 101100 – 1.
+После приёма слова производится его обработка. При этом проверяется сумма его разрядов,
+включая контрольный. Если она нечётна, это означает, что при передаче этого слова произошёл сбой,
+и оно автоматически заменяется на зарезервированное слово {zeroes}. Если она чётна, это означает,
+что сбоя не было или сбоев было больше одного. В этом случае принятое слово не изменяется.
+Исходное сообщение: <b>{original_text}</b> было принято в виде: <b>{received_text}</b>.
+Как будет выглядеть принятое сообщение после обработки?'''
 
         variants = wrong_idx
         variants.insert(0, correct_idx)
@@ -410,11 +419,12 @@ class InfSize(SingleChoice):
         pow = self.rnd.in_range(4, 7)
         value = 2 ** pow
         time = self.rnd.in_range(2, 11)
-        self.text = f'''Известно, что длительность непрерывного подключения к сети Интернет с помощью модема
-            для некоторых АТС не превышает {time} минут.
-            Определите максимальный размер файла (в Килобайтах),
-            который может быть передан за время такого подключения,
-            если модем передает информацию в среднем со скоростью {value} Килобит/с?'''
+        self.text = f'''
+Известно, что длительность непрерывного подключения к сети Интернет с помощью модема
+для некоторых АТС не превышает {time} минут.
+Определите максимальный размер файла (в Килобайтах),
+который может быть передан за время такого подключения,
+если модем передает информацию в среднем со скоростью {value} Килобит/с?'''
         time *= 60
         self.set_variants([
             2 ** (pow - 3) * time,
