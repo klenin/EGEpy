c1058b33e58dacc1cbfd6f406306288c2492bf5a
saver <saversaversaversaver@gmail.com>
2022-01-22 20:05:29 +1000

    wip

--- EGE/SQL/Table.py
+++ EGE/SQL/Table.py
@@ -1,10 +1,12 @@
 from ..GenBase import EGEError
+from ..Prog import CallFuncAggregate
+from ..Utils import aggregate_function
 
 class Field:
     name: str
     name_alias: str
 
-    def __int__(self, attr):
+    def __init__(self, attr):
         if isinstance(attr, dict):
             self.name = attr['name']
             self.name = attr['name_alias']
@@ -30,9 +32,51 @@ class Table:
             i.table = self
 
     def _make_field(self, field):
-        return Field(field)
+        return field if isinstance(field, Field) else Field(field)
 
+    def _update_field_index(self):
+        for i, key in enumerate(self.fields):
+            self.field_index[i] = i
 
+    def fields(self):
+        return self.fields
 
+    def insert_row(self, fields):
+        if len(fields) != len(self.fields):
+            EGEError(f'Wrong column count {len(fields)} != {len(self.fields)}')
+        self.data.append(fields)
+        return self
 
+    def _hash(self):
+        env = {}
+        env['&columns'] =  +{ map { $_ => $self->column_array($_) } @{$self->{fields}} };
+        env['&'] = +{ map { $_ => 'EGE::SQL::Table::Aggregate::' . $_ } aggregate_function };
+        env['&count'] = self.count()
+        return env
 
+    def select(self, fields, where, p):
+        ref, aggr, group, having = 0, 0, 0, 0
+        if isinstance(ref, dict):
+            ref = p[ref]
+            group = p[group]
+            having = p[having]
+
+        aggr = list(filter(lambda x: isinstance(x, CallFuncAggregate), fields))
+        k = 0
+
+        result = Table([ map { not isinstance(i, Field) and ref $_ ? 'expr_' . ++$k : $_ } @$fields ]);
+
+        values = map { ref $_ ? $_ : make_expr($_) } @$fields
+        calc_row = lambda x: [ i.run(x) for i in values ]
+
+        tab_where = self.where(where, ref)
+        if group:
+            result[data] = tab_where.group_by(calc_row, group, having)
+        else:
+            ans = []
+            env = tab_where._hash()
+            for i in tab_where[data]:
+                ans.append([ calc_row(tab_where._row_hash(i, env)) ])
+            result[data] = [ ans[0] ] if aggr else [ ans ]
+
+        return result
