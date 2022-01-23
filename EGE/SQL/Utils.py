938ccfa9d329a87fdceed2ef67fad7d6258bddf7
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2022-01-24 02:12:49 +1000

    Add first tests for sql and add partial implementation of sql module
    https://www.notion.so/SQL-494243006ef04f68b803fff9104e5597

---
+++ EGE/SQL/Utils.py
@@ -0,0 +1,16 @@
+from EGE.Random import Random
+# from EGE.SQL.Table import Aggregate
+
+def create_table(rnd: Random, row: int, col: int, name: str):
+    product = None
+    return product
+
+# def aggregate_function(name=None):
+#     """name: Union[str, None]"""
+#     aggr = Aggregate.__subclasses__()
+#     if name is not None:
+#         for sub in aggr:
+#             if name == sub.__name__:
+#                 return sub
+#     return [sub for sub in aggr]
+
