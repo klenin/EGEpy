184fa262377cc36720a027f1859d4a4836bfdd08
Artem Sityaev <luckthemall@gmail.com>
2022-04-27 00:56:29 +1000

    EGE2022_08: Add BlackWhiteBalls task

--- EGE/Gen/EGE2022/Z08.py
+++ EGE/Gen/EGE2022/Z08.py
@@ -1,5 +1,5 @@
 from ...GenBase import DirectInput
-from math import ceil, log
+from math import ceil, log, log2
 
 
 class CellEncoding(DirectInput):
@@ -32,5 +32,17 @@ class TicTacToe(CellEncoding):
         rows = cols
         self.correct = self._resolve(cols * rows)
         self.text = f"Двое играют в «крестики-нолики» на поле {cols} на {rows} клетки. Какое количество информации (в битах) получил второй игрок, узнав ход первого игрока?"
+
+        return self
+
+
+class BlackWhiteBalls(DirectInput):
+    def generate(self):
+        prob_inverse = 2 ** self.rnd.in_range(2, 10)
+        black = self.rnd.in_range(2, 10)
+        white = prob_inverse * black - black
+
+        self.correct = ceil(log2(prob_inverse))
+        self.text = f"В корзине лежат {black} черных шаров и {white} белых. Сколько бит информации несет сообщение о том, что достали черный шар?"
 
         return self
