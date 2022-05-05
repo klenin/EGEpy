0b06149112ed688950a8daebcb88566d2cd07551
Anton Pochuvalov <podchuvalovvl@gmail.com>
2022-05-05 22:15:18 +1000

    Add new gen_2022.py file for EGE2022 problems

--- gen.py
+++ gen.py
@@ -8,8 +8,6 @@ from EGE.Gen.EGE import A01, A02, A03, A04, A05, A06, A07, A08, A09, A10, A11, A
 from EGE.Gen.EGE import B01, B02, B03, B04, B05, B06, B07, B08, B10, B11, B12, B13, B14, B15
 from EGE.Gen.EGE import Z06, Z09, Z10, Z11, Z12, Z13, Z15, Z16, Z18, Z22
 
-from EGE.Gen.EGE2022 import N03, N05, N06, N07, N08, N11, N14
-
 rnd = EGE.Random.Random(2342134)
 
 questions = [q.generate() for q in [
@@ -128,70 +126,6 @@ questions = [q.generate() for q in [
     Z22.CalculatorFindPrgmCount(rnd),
 ]]
 
-questions_2022 = [q.generate() for q in [
-    N03.GenDatabase(rnd),
-    N05.FindNumber(rnd),
-    N05.MinAddDigits(rnd),
-    N05.Draftsman(rnd),
-    N05.RobotMigrant(rnd),
-    N05.RobotAndIronCurtain(rnd),
-    N05.Grasshopper(rnd),
-    N05.CalculatorBothWays(rnd),
-    N05.FourDigitNumber(rnd),
-    N05.ThreeDigitNumber(rnd),
-    N05.FiveDigitNumber(rnd),
-    N05.NaturalNumber(rnd),
-    N05.FourDigitOddNumber(rnd),
-    N05.RemainderOfDivision(rnd),
-    N05.RemoveLastBit(rnd),
-    N05.EightBitNumber(rnd),
-    N05.EvenOddNumber(rnd),
-    N05.BitsSumRemainder(rnd),
-    N05.EvenOddBitsSum(rnd),
-    N05.ReverseBits(rnd),
-    N05.ComparingZerosAndOnes(rnd),
-    N05.TernaryNumber(rnd),
-    N06.SumOfTwoLinearFunctions(rnd),
-    N06.ArithmeticProgression(rnd),
-    N06.TwoLinearFunctions(rnd),
-    N06.InputTwoLinearFunctions(rnd),
-    N07.ImageTransferTime(rnd),
-    N07.ImageStorageSize(rnd),
-    N07.ImageStoragePalette(rnd),
-    N07.ImageStorageResizePalette(rnd),
-    N07.ImageStoragePicturesN(rnd),
-    N07.ImageStoragePicturesNForPeriod(rnd),
-    N07.ImageStoragePicturesNPalette(rnd),
-    N07.ImageStorageDpiSize(rnd),
-    N07.ImageStorageDpiResize(rnd),
-    N07.TextTransferTime(rnd),
-    N07.TextTransferDataLength(rnd),
-    N07.TextFileResizeDiff(rnd),
-    N07.TextFileResizeSymbolsN(rnd),
-    N08.ChessCellEncoding(rnd),
-    N08.PositiveInts(rnd),
-    N08.TicTacToe(rnd),
-    N08.BlackWhiteBalls(rnd),
-    N08.BlackWhiteBalls2(rnd),
-    N08.Pencils(rnd),
-    N08.Pencils2(rnd),
-    N08.VasyaMarks(rnd),
-    N08.WordCount(rnd),
-    N08.WordCount2(rnd),
-    N08.LightPanel(rnd),
-    N08.LightPanel2(rnd),
-    N08.WordsWithRestrictions(rnd),
-    N08.WordEncoding(rnd),
-    N08.WordEncoding2(rnd),
-    N08.WordEncoding3(rnd),
-    N08.WordEncoding4(rnd),
-    N11.AmountOfInformationSport(rnd),
-    N11.AmountOfInformationPasswordsExtra(rnd),
-    N11.AmountOfInformationPasswords(rnd),
-    N11.AmountOfInformationCars(rnd),
-    N14.DirectSumDigits(rnd),
-]]
-
 if not sys.stdout.isatty():
     sys.stdout.reconfigure(encoding='utf-8') # type: ignore
-print(EGE.Html.make_html(questions_2022))
+print(EGE.Html.make_html(questions))
